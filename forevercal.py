beb4f7b9a64ac41ce0fe9413d17677dba41ff7de
Johnson Ngao <developerjon@outlook.com>
2023-06-12 01:36:39 -0400

    Add files via upload

--- forevercal.py
+++ forevercal.py
@@ -1,4 +1,57 @@
 
+"""
+NAME:
+----
+forevercal, named to briefly describe its main functionality, determine the 
+weekday of any given date; hence, its named forevercal after forever calendar.
+
+MOTIVE:
+------
+The main motive of this module is to improve explicitness from the built-in 
+python weekday interface (calendar.weekday).
+
+Python weekday hardcodes its index => weekday mapper: 0 => 'Mon', 1 => 'Tue', 2 
+=> 'Wed', ... etc. Compared to this module, that mapper is True for genesis 
+date 1/1/1. However, this module offers more flexibility to specify a custom 
+genesis date (Yes, even today) with args (y, m, d, weekday) to *Date 
+interfaces. The genesis date derives a new mapper that's compatible with any 
+specified subject date (even earlier than the genesis date).
+
+DESCRIPTION:
+-----------
+This module implements an algorithm for determining the weekday (Sun, Mon, 
+Tue, ... etc) of any date given a known weekday of a prime date (genesis date). 
+The algorithm was developed by the author of this module from scratch and 
+tested locally, see the tests.py file and top level code in this file. 
+
+Presentable* classes are the recommended interfaces to use because they are the 
+highest level interfaces, easy to use, and follow a simplicity approach. They 
+are also reusable to various front ends: console, GUI, Web, ... etc. They 
+define __str__ for printing. Presentable* month and year interfaces define 
+__iter__ which makes them iterable, yielding weeks on demand. Ideal for use 
+in GUI and Web frontends since __str__ may not be suitable. 
+
+Values passed to the year, month, and date interfaces of this module are 
+validated. For instance, invalid year, month, and day values raise OutOfRange 
+instances if they are out of expected range (see their respective classes code 
+for implementation). However, users should remember to handle these errors 
+for better flow management. 
+
+USAGE:
+-----
+This module can be imported just like any other python module with no 
+dependencies, or can be run as a top level file and get specific output 
+depending on the CLI argument specified (-y yearnum, -m monthnum, -d daynum). 
+Run 'python forevercal.py -h' for help on top level script usage. 
+
+TBD:
+---
+Hybrid interfaces that imply both the Julian and Gregorian calendar features 
+(ideal for per country calendars which have transitioned from one form to 
+another).
+"""
+
+
 #######################################################################
 #   DEFAULTS (standard), customize how you want. See respective classes 
 # to see how.
@@ -569,43 +622,96 @@ class PresentableGregorian(Gregorian): pass
 # Top Level code
 #####################################################
 if __name__ == '__main__':
-    from argparse import ArgumentParser
+    import unittest, sys
     from datetime import datetime
-    
-    # CLI args
-    parser = ArgumentParser()
-    parser.add_argument('-y', type=int, help='prints the year.')
-    parser.add_argument('-m', type=int, help='must pass -y. prints the month.')
-    parser.add_argument('-d', type=int, help='must pass -y and -m. prints the date.')
-    args = parser.parse_args()
-    y, m, d = args.y, args.m, args.d
-    
-    today = datetime.today()
-    date  = PresentableDate(today.year, today.month, today.day,
-                # hard coded python weekday index mapper
-                ['Monday',
-                 'Tuesday', 
-                 'Wednesday', 
-                 'Thursday', 
-                 'Friday', 
-                 'Saturday', 
-                 'Sunday'][today.weekday()])
-    
-    # print date
-    if y and m and d:
-        print(date(y, m, d))
+    from forevercal import (Date, Gregorian as Greg, Month, 
+                            OutOfRange)
+
+    class ForeverCalTests(unittest.TestCase):
+        def setUp(self): print()
         
-    # print month
-    elif y and m:
-        wday1  = date(y, m, 1).weekday()
-        isleap = Gregorian(y).isleap()
-        print(PresentableMonth(m, isleap, wday1))
-        
-    # print year
-    elif y:
-        wday1 = date(y, 1, 1).weekday()
-        print(PresentableGregorian(y, wday1))
-    elif y:
-        wday1 = date(y, 1, 1).weekday()
-        print(PresentableGregorian(y, wday1))
-    
+        def test_date_attrs(self):
+            today   = datetime.today()
+            y, m, d = today.year, today.month, today.day
+            date = Date(y, m, d)
+            for values in zip([date.year, date.month, date.day], 
+                            [y, m, d]):
+                self.assertEqual(*values)
+            def func(n): date.year = n
+            self.assertRaises(Exception, func, 0)
+
+        def test_in_range(self):
+            """
+            Testing if the values passed are in range.
+
+            In these, year = 0, month = 0 or 13, day = 0 or 32.
+            """
+            self.assertRaises(OutOfRange, Greg,  0)             # year  = 0
+            self.assertRaises(OutOfRange, Month, 0,  False)     # month = 0
+            self.assertRaises(OutOfRange, Month, 13, False)     # month = 13
+            self.assertRaises(OutOfRange, Date,  1, 1, 0)       # day   = 0
+            self.assertRaises(OutOfRange, Date,  1, 1, 32)      # day   = 32
+            
+        def test_yrdays(self):
+            "testing if common and leap years are right"
+            self.assertEqual(Greg(4).days(), 366)
+            self.assertEqual(Greg(5).days(), 365)
+
+
+        def test_weekday(self):
+            """
+            Testing if forevercal weekday == pyhton weekday.
+            All core interfaces for Julian, Month, and Date
+            are used implicitly (Gregorian is used).
+            
+            Each derived date derives the next date's weekday
+
+            -v CLI arg enables test verbosity. It prints
+            current century year tested.
+
+            This is the core test. If this fails, nothing 
+            else in the interface matters.
+            """
+            import calendar
+            
+            today = datetime.today()
+            date  = Date(today.year, today.month, today.day,
+                        ['Monday', 
+                        'Tuesday', 
+                        'Wednesday', 
+                        'Thursday', 
+                        'Friday', 
+                        'Saturday', 
+                        'Sunday'][today.weekday()])     # true genesis
+
+            # test logic
+            century = 1                              
+            for y in range(1, datetime.today().year + 1, 1):
+
+                # -v (verbosity)
+                global verbose
+                if verbose and ((y < 100 and century == 1) or 
+                                y % 100 == 1):
+                    print('century:', century)
+                    century += 1
+
+                Y = Greg(y)                                    # year
+                for m in range(1, 12 + 1, 1):
+                    M = Month(m, Y.isleap())                   # month
+                    for d in range(1, M.days() + 1, 1):
+                        date       = date(y, m, d)             # date
+                        fc_weekday = date.weekday()
+                        # fc_weekday = date.weekday(y, m, d)   # alt
+                        py_weekday = calendar.weekday(y, m, d)
+                        py_weekday = ['Monday',                # weekday
+                                    'Tuesday',                 # names
+                                    'Wednesday',
+                                    'Thursday',
+                                    'Friday',
+                                    'Saturday',
+                                    'Sunday'][py_weekday]
+                        # test operation
+                        self.assertEqual(
+                            fc_weekday.lower(), py_weekday.lower())
+    verbose = True if '-v' in sys.argv else False
+    unittest.main()
